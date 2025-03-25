846afa729c6b394acfc7c3e0c47b3af4e67b3666
Sára Dzurechová <dzurechova.s@gmail.com>
2025-03-25 16:02:21 +0100

    spracovanie formulara do databazy

---
+++ db/spracovanieFormulara.php
@@ -0,0 +1,37 @@
+<?php
+$host = "localhost";
+$dbname = "formular";
+$port = 3306;
+$username = "root";
+$password = "";
+
+$options = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    );
+
+// Pripojenie PDO
+try {
+    $conn = new PDO('mysql:host='.$host.';dbname='.$dbname.";port=".$port, $username,
+        $password, $options);
+} catch (PDOException $e) {
+    die("Chyba pripojenia: " . $e->getMessage());
+}
+
+$meno = $_GET["meno"];
+$email = $_GET["email"];
+$sprava = $_GET["sprava"];
+// SQL príkaz INSERT
+$sql = "INSERT INTO udaje (meno, email, sprava)
+    VALUE ('".$meno."', '".$email."', '".$sprava."')";
+$statement = $conn->prepare($sql);
+try {
+    $insert = $statement->execute();
+    header("Location: http://localhost/sablona/thankyou.php");
+    return $insert;
+} catch (\Exception $exception) {
+    return false;
+}
+// Zatvorenie pripojenia
+$conn = null;
+
--- kontakt.php
+++ kontakt.php
@@ -25,15 +25,15 @@ include('parts/header.php');
           <p>Consectetur laborum aliqua nulla anim cupidatat consectetur est veniam cupidatat.</p>
         </div>
         <div class="col-50 text-right">
-          <h3>Napíšte nám</h3>
-          <form id="contact" action="thankyou.html">
-            <input type="text" placeholder="Vaše meno" id ="meno"  required><br>
-            <input type="email" placeholder="Váš email" id="email" required><br>
-            <textarea name="" placeholder="Vaša správa" id="sprava"></textarea><br>
-            <input type="checkbox" name="" id="" required>
-            <label for=""> Súhlasím so spracovaním osobných údajov.</label><br>
-            <input type="submit" value="Odoslať">
-          </form>
+            <h3>Napíšte nám</h3>
+            <form id="contact" method="post" action="db/spracovanieFormulara.php">
+                <input type="text" placeholder="Vaše meno" id ="meno"  required><br>
+                <input type="email" placeholder="Váš email" id="email" required><br>
+                <textarea name="" placeholder="Vaša správa" id="sprava"></textarea><br>
+                <input type="checkbox" name="" id="" required>
+                <label for=""> Súhlasím so spracovaním osobných údajov.</label><br>
+                <input type="submit" value="Odoslať">
+            </form>
         </div>
       </div>
     </section>
